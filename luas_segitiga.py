ba08453f741651ea240aa98664cf3607f1d999c4
seanganteng <48561347+seanganteng@users.noreply.github.com>
2022-01-15 22:14:39 +0700

    Create luas_segitiga.py

---
+++ luas_segitiga.py
@@ -0,0 +1,7 @@
+print ('PENGHITUNG LUAS SEGITIGA')
+angka1 = float (input ("masukan alas segitiga : "))
+angka2 = float  (input ("masukan tinggi segitiga : "))
+
+print ('hasil', angka1 * angka2 * 1/2 ,"cm2")
+
+
